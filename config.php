e4c42da9a0fb9c18d967bac2c8cd2715e798bf22
MikyalFaizin <mikyalfaizin23@gmail.com>
2024-10-24 16:19:38 +0700

    Upload App Nilai

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$hostdb = "localhost";
+$userdb = "root";
+$passdb = "tkjtkj";
+$namedb = "assesment_31";
+$con = mysqli_connect($hostdb,$userdb,$passdb,$namedb);
---
+++ home.php
@@ -0,0 +1,17 @@
+<div class="container mt-3 mb-5">
+        <div class="card">
+    <div class="card-body">
+        <p><img src="images/kelastkj.png" alt="rgi" class="card-img rounded">
+            <p>Kelas TKJ merupakan kelasnya para programer bagi anda yang mau menjadi seorang programmer yang handal, dikelas ini kamu akan di ajarkan skill seputar dunia Komputer dan Jaringannya mulai dari belajar mikrotik, server, dan membuat Web Development.
+                <ol>
+                    <li>Mikrotik membahas tentang bagaimana menghubungkan komputer ke internet</li>
+                    <li>Server membahas bagaimana menjalankan server menggunakan RPM Base</li>
+                    <li>Web development membahas bagaimana membuat dan merancang sebuah web</li>
+                    <li>Hardware & software membahas bagaimana merakit sebuah PC serta menginstal aplikasi</li>
+                    <li>Menulis Kreatif</li>
+                </ol>
+                Lulusan teknik computer dan jaringan menghasilkan para programmer handal yang nantinya akan bergelut didunia IT alumni lulusan TKJ banyak yang bekerja di perusahaan IT dan beberapa lembaga yang membutuhkan keahlian mengenai IT.
+            </p>
+        </p>
+    </div>
+</div>
